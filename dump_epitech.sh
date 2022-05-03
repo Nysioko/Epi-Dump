cbdc5bb25485806e3a72d452f7a782e7079d7e6c
Nysioko <guilhem.jehanno@epitech.eu>
2022-05-03 15:42:05 +0200

    [+] - Added resources homebrew for mac and updated file

--- dump_epitech.sh
+++ dump_epitech.sh
@@ -1,129 +1,91 @@
 #!/bin/bash
 clear
-echo "#########################################################################"
-echo "####################### DUMP EPITECH DEBIAN #############################"
-echo "#########################################################################"
+echo "   ____       _       ___                   "
+echo "  / __/___   (_)____ / _ \ __ __ __ _   ___ "
+echo " / _/ / _ \ / //___// // // // //    \ / _ \ "
+echo "/___// .__//_/     /____/ \_,_//_/_/_// .__/"
+echo "    /_/                              /_/    "
+
 if [ "$EUID" -ne 0 ]
-  then echo "The script must be run as root. Please use sudo and run the script again."
+  then echo -e "\e[31mPlease run as root\e[0m"
   exit
 fi
 if curl -s --head  --request GET www.google.com | grep "200 OK" > /dev/null ; then
     echo "You are connected to the internet, the script will continue"
 else
     echo "You are not connected to the internet, the script will stop. Please connect to the internet and run the script again"
-fi
-if grep -q "debian" /etc/os-release | grep -q "ubuntu" /etc/os-release; then
-    echo "You are using Debian, the script will continue"
-else
-    echo "You are not using Debian, the script will stop. Please use Debian and run the script again"
+    exit
 fi
 
-echo "Yeah, you are ready to go, let's start the script"
+echo ""
 
-read -p "Please enter your {Epitech.} login: " login
+echo -e "\e[33mChecking your system\e[0m"
 
-echo "Adding PPAs"
-
-echo "The script will now install the dependencies; this may take a while, please be patient..."
-
-echo "Changing directory to /tmp and downloading the dependencies"
-cd /tmp
-
-## Update the system
-apt-get update
-echo "done."
-
-apt-get upgrade
-echo "done."
-
-## Install the dependencies
-
-apt-get install -y wget
-echo "done."
-
-apt-get install -y curl
-echo "done."
-
-apt-get install -y git
-echo "done."
-
-apt-get install -y vim
-echo "done."
-
-apt-get install emacs-nox
-echo "done."
-
-apt-get install -y python-pip
-echo "done."
-
-apt-get install -y python
-echo "done."
-
-apt-get install -y python-setuptools
-echo "done."
-
-apt-get install -y python-virtualenv
-echo "done."
-
-apt-get install -y valgrind
-echo "done."
-
-apt-get install -y libxml2-dev
-echo "done."
-
-apt-get install -y libxslt-dev
-echo "done."
-
-apt-get install -y libjpeg-dev
-echo "done."
-
-apt-get install -y libpng-dev
-echo "done."
-
-apt-get install -y libfreetype6-dev
-echo "done."
-
-apt-get install -y libssl-dev
-echo "done."
-
-apt-get install -y libffi-dev
-echo "done."
-
-apt-get install -y libxmlsec1-dev
-echo "done."
-
-apt-get install -y libxmlsec1-openssl
-echo "done."
-
-apt-get install -y build-essential
-echo "done."
-
-apt-get install -y libpq-dev
-echo "done."
-
-apt-get install -y libpq5
-echo "done."
-
-apt-get install -y lncurses-dev
-echo "done."
-
-apt-get install -y libncurses5-dev
-echo "done."
-
-apt-get install -y libncursesw5-dev
-echo "done."
-
-apt-get install -y ghc
-echo "done."
-
-if read -t 5 -p "Do you want generate a SSH key for GitHub? (y/n) " answer; then
-    if [ "$answer" = "y" ]; then
-        echo "Generating SSH key for GitHub..."
-        ssh-keygen -t rsa -b 4096 -C $login
-        echo "done."
-    else
-        echo "Ok, we won't generate a SSH key for GitHub"
-    fi
+#Detect linux distrib
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    OS=SuSE
+    VER=$(cat /etc/SuSe-release)
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    OS=RedHat
+    VER=$(cat /etc/redhat-release)
+elif [ -f /usr/bin/sw_vers ]; then
+    OS=MacOS
+    VER=$(sw_vers -productVersion)
 else
-    echo "You chose not to install the requirements"
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+    echo -e "\e[31mYour OS is not supported by this script, please contact the author of this script\e[0m"
+fi
+sleep 1
+echo -e "\e[32mOS: $OS\e[0m"
+echo -e "\e[33mVersion: $VER\e[0m"
+
+echo ""
+echo -e "\e[32mCompatible distrib detected\e[0m"
+sleep 1
+echo ""
+
+echo "Please enter your email:" ; read email
+
+if [[ $email = *"@epitech.eu"* ]]; then
+    echo -e "\e[32mYour email seems to be valid\e[0m"
+else
+    echo -e "\e[31mYour need to enter an epitech email\e[0m"
+    exit
+fi
+
+#Install dependencies for linuxbase.org
+if [ "$OS" = "Linux" ]; then
+    echo -e "\e[33mInstalling dependencies\e[0m"
+    sleep 1
+    apt-get update
+    apt-get install -y wget curl git
+fi
+
+if [ "$OS" = "MacOS" ]; then
+    echo -e "\e[33mInstalling dependencies\e[0m"
+    echo -e "\e[32mInstalling Homebrew\e[0m"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install wget curl git
 fi
