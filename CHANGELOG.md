83940ba524f2d4641368ead3480c93aec0943719
Nysioko <nysioko@gmail.com>
2022-05-23 23:05:20 +0200

    [+] - Script improvement, asking directrly sudo right before starting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # 🕒 Changelog
 
+## Pre-Alpha 0.3.1a
+### Date: 23 May 2022
+Script improvement, asking directrly sudo right before starting, and adding a check for the presence of the `sudo` command.
+This is a temporary fix for the issue of the script, because somes commands won't work with the `sudo` right.
+
 ## Pre-Alpha 0.3a
 ### Date: 23 May 2022
 Added menu for Epi-Dump, a function who install the missing packages, added installation of mrclean
--- epi-dump.py
+++ epi-dump.py
@@ -5,9 +5,10 @@ import os
 import urllib.request
 import urllib.parse
 
+
 def init_script():
-    if os.geteuid() != 0:
-        print("You need to be root to run this script.")
+    if os.geteuid() == 0:
+        print("Don't run this script as root!")
         sys.exit(1)
     else:
         if os.system('command -v git > /dev/null 2>&1') != 0:
@@ -24,6 +25,7 @@ def init_script():
                 version = open('.version').read()
                 return version
 
+
 def splash(version):
     print("")
     print("\033[1;34m   ____       _       ___                   \033[0m")
@@ -33,10 +35,16 @@ def splash(version):
     print("\033[1;34m    /_/                               /_/    \033[0m\n\n")
     print("\033[1;32m+ -- -- +=[ Script by: Nysioko\033[1;m")
     print("\033[1;32m+ -- -- +=[ Version: " + version + "\033[1;m")
-    print("\033[1;32m+ -- -- +=[ Last update: 23 May 2022\033[1;m")
+    os.system(
+        "git log --pretty=format:'%ad' --date=short -1 > resources/.last_commit")
+    last_commit = open("resources/.last_commit").read()
+    print("\033[1;32m+ -- -- +=[ Last commit: " + last_commit + "\033[1;m")
+    os.system("rm resources/.last_commit")
     print("\033[1;32m+ -- -- +=[ This script is under the Unlicense\033[1;m\n")
     print("\033[1;91m[W] This script is under development, please report any bug to @Nysioko\033[0m")
-    print("\033[1;91m[W] This script modify your environment, please be careful with it\033[0m\n")
+    print(
+        "\033[1;91m[W] This script modify your environment, please be careful with it\033[0m\n")
+
 
 def check_internet():
     try:
@@ -45,10 +53,12 @@ def check_internet():
     except:
         return False
 
+
 def restart_script():
     print('Restarting script...\n')
     os.system('sleep 1')
     os.system('sudo python3 ./epi-dump.py')
+
 
 def help_func(version):
     print('\n\033[1;32m[*] Usage:\033[0m')
@@ -66,27 +76,28 @@ def help_func(version):
     splash(version)
     menu(version)
 
+
 def is_latest_version(version):
     print('Checking script version...')
-    os.system("wget -O /tmp/.version https://raw.githubusercontent.com/Nysioko/EpiDump/main/.version > /dev/null 2>&1")
-    if os.path.isfile('/tmp/.version') == False:
+    os.system("wget -O resources/.version https://raw.githubusercontent.com/Nysioko/EpiDump/main/.version > /dev/null 2>&1")
+    if os.path.isfile('resources/.version') == False:
         print(
             '\033[1;31m[!] Impossible to check version, please try again later\033[0m')
         print(
             '\033[1;31m[!] If the problem persist, please report it to @Nysioko\033[0m')
         sys.exit(1)
-    online_version = open("/tmp/.version").read()
+    online_version = open("resources/.version").read()
 
     if online_version == version:
         print(
             '\033[1;32m[+] You are using the latest version of the script.\033[0m\n')
-        os.system('rm /tmp/.version')
+        os.system('rm resources/.version')
     else:
         if (version > online_version):
             print(
                 '\033[1;31m[!] This version is too highest in comparison to the online version.\033[0m')
             print('\033[1;33m[!] Pulling the latest version...\033[0m\n')
-            os.system('rm /tmp/.version')
+            os.system('rm resources/.version')
             # restart_script()
         else:
             print(
@@ -94,10 +105,11 @@ def is_latest_version(version):
             print('\033[1;31m[!] Actual version: \033[1;32m' +
                   version + '\033[0m')
             print('\033[1;31m[!] Latest version: \033[1;32m' +
-                  open("/tmp/.version").read() + '\033[0m')
+                  open("resources/.version").read() + '\033[0m')
             print('\033[1;33m[!] Pulling the latest version...\033[0m\n')
-            os.system('rm /tmp/.version')
+            os.system('rm resources/.version')
             # restart_script()
+
 
 def detect_package_manager():
     print('\nDetecting package manager...')
@@ -123,6 +135,7 @@ def detect_package_manager():
         print('\033[1;31mUnknown\033[0m')
         return package_manager
 
+
 def ask_install_missing_package(package_manager, i):
     print('\033[1;33m[!] Do you want to install them now? (y/n)\033[0m')
     answer = input('\033[1;33m[?] Answer: \033[0m')
@@ -146,6 +159,7 @@ def ask_install_missing_package(package_manager, i):
         print('\n\033[1;31m[!] Please answer with y or n.\033[0m\n')
         ask_install_missing_package(package_manager, i)
 
+
 def check_dependencies(package_manager):
     dependancies = ['wget', 'curl', 'gcc', 'g++', 'cmatrix']
     print('Checking dependencies...')
@@ -163,16 +177,27 @@ def check_dependencies(package_manager):
                   i + '" to continue.\033[0m')
             ask_install_missing_package(package_manager, i)
 
+
+def chain(version):
+    os.system("clear")
+    splash(version)
+    is_latest_version(version)
+    menu(version)
+
+
 def about(version):
     print("\n\033[1;32m+ -- -- +=[ Script by: Nysioko\033[1;m")
     print("\033[1;32m+ -- -- +=[ Version: " + version + "\033[1;m")
-    print("\033[1;32m+ -- -- +=[ Last update: 19 May 2022\033[1;m")
+    os.system(
+        "git log --pretty=format:'%ad' --date=short -1 > resources/.last_commit")
+    last_commit = open("resources/.last_commit").read()
+    print("\033[1;32m+ -- -- +=[ Last commit: " + last_commit + "\033[1;m")
+    os.system("rm resources/.last_commit")
     print("\033[1;32m+ -- -- +=[ This script is under the Unlicense\033[1;m\n")
     print("\033[1;32m[+] Press Enter to continue\033[0m")
     input()
-    os.system("clear")
-    splash(version)
-    menu(version)
+    chain(version)
+
 
 def install(version):
     check_dependencies(detect_package_manager())
@@ -182,11 +207,13 @@ def install(version):
         os.system('sudo chmod +x /usr/bin/mrclean')
         print('\n\033[1;32m[+] mrclean successfully installed.\033[0m')
         print('\033[1;32m[+] You can now use mrclean.\033[0m\n')
-        os.system('sleep 3 ; clear')
-        splash(version)
-        menu(version)
+        os.system('sleep 3')
+        chain(version)
     else:
         print('\n\033[1;32m[+] mrclean not installed.\033[0m\n')
+        os.system('sleep 3')
+        chain(version)
+
 
 def menu(version):
     print('\033[1;32m[+] Select an option: \033[0m')
@@ -197,16 +224,16 @@ def menu(version):
 
     option = input('\033[1;34mEpi-Dump ➜ \033[0m')
     if option == '1' or option == 'install' or option == 'Install' \
-        or option == 'INSTALL' or option == 'i' or option == 'I':
+            or option == 'INSTALL' or option == 'i' or option == 'I':
         install(version)
     elif option == '2' or option == 'help' or option == 'Help' or \
-        option == 'HELP' or option == 'h' or option == 'H':
+            option == 'HELP' or option == 'h' or option == 'H':
         help_func(version)
     elif option == '4' or option == 'about' or option == 'About' or option == 'ABOUT':
         about(version)
     elif option == '5' or option == 'exit' or option == 'quit' or option == 'q' \
-        or option == 'Q' or option == 'Exit' or option == 'Quit' or option == 'EXIT' \
-        or option == 'QUIT':
+            or option == 'Q' or option == 'Exit' or option == 'Quit' or option == 'EXIT' \
+            or option == 'QUIT':
         print('\033[1;32m[+] Bye!\033[0m')
         sys.exit(0)
     else:
@@ -214,6 +241,23 @@ def menu(version):
         os.system("sleep 1 ; clear")
         splash(version)
         menu(version)
+
+
+def ask_sudo(version):
+    print('\033[1;32m[+] Please enter your sudo password: \033[0m')
+    os.system('stty -echo')
+    password = input()
+    os.system('stty echo')
+    print('\n')
+    if os.system('echo ' + password + ' | sudo -S whoami > /dev/null 2>&1') == 0:
+        print('\033[1;32m[+] Password accepted\033[0m')
+        chain(version)
+        return True
+    else:
+        os.system('stty -echo')
+        print('\033[1;31m[!] Wrong password\033[0m')
+        sys.exit(1)
+
 
 def main():
     os.system('clear')
@@ -226,10 +270,11 @@ def main():
         sys.exit(1)
     try:
         while True:
-            menu(init_script())
+            ask_sudo(init_script())
     except KeyboardInterrupt:
         print('\n\033[1;32m[+] Ctrl + C pressed, Bye!\033[0m')
         sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
